cb48d04f6d93be3e5f7b787afeeb9da73ee57fad
GonzaloUlla <gonzaulla@gmail.com>
2020-04-23 03:09:57 -0300

    Close Issue #14 and Close Issue #15

--- news-crawler/entrypoint.sh
+++ news-crawler/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-service cron start
-service filebeat start
-cd /usr/src/app || exit 1
-nohup python -m spiders.websites spiders/websites.py 2>&1 | tee -a logs/websites.log &
+service cron start || exit 1
+service filebeat start || exit 1
+supervisord -n -c /etc/supervisor/supervisord.conf 2>&1 | tee -a /usr/src/app/logs/supervisor.log &
 tail -f /dev/null
